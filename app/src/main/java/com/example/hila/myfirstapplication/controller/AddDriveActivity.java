8c13965f51e7fee90c31bfb3bc03285161d5653c
hilaShamay <hilashamay4@gmail.com>
2018-11-20 18:56:29 +0200

    version 4

--- app/src/main/java/com/example/hila/myfirstapplication/controller/AddDriveActivity.java
+++ app/src/main/java/com/example/hila/myfirstapplication/controller/AddDriveActivity.java
@@ -2,6 +2,8 @@ package com.example.hila.myfirstapplication.controller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.hila.myfirstapplication.R;
 
@@ -11,5 +13,10 @@ public class AddDriveActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_drive);
+        Spinner spinner=findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, android.R.array.,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 }

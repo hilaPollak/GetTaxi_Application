a563b3a870efbe33b9b66668bc9c1cb0d82e9a5e
hilaShamay <hilashamay4@gmail.com>
2018-11-21 19:11:53 +0200

    version 6

--- app/src/main/java/com/example/hila/myfirstapplication/controller/AddDriveActivity.java
+++ app/src/main/java/com/example/hila/myfirstapplication/controller/AddDriveActivity.java
@@ -2,21 +2,41 @@ package com.example.hila.myfirstapplication.controller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.hila.myfirstapplication.R;
 
-public class AddDriveActivity extends Activity {
+public class AddDriveActivity extends Activity  implements  AdapterView.OnItemSelectedListener {
+    String[] country = {"AVAILABLE", "TREATMENT", "ENDING"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_drive);
-        Spinner spinner=findViewById(R.id.spinner1);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, android.R.array.,android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(this);
 
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, country);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+
+    }
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(), country[position], Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 }
